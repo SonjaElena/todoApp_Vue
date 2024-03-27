93f5219f34dc8315d81eaf88925b5a751b58b000
SonjaElena <sonja.koppenhoefer@web.de>
2024-03-27 17:01:15 +0100

    feat: add and remove work

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 Vue.createApp({
   data() {
-    return {};
+    return {
+      todos: [],
+      newTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== "") {
+        this.todos.push({
+          id: Math.floor(Math.random() * Date.now()),
+          description: this.newTodo,
+          done: false,
+        });
+        this.newTodo = "";
+      }
+    },
+    toggleStatus(index) {
+      this.todos[index].done = !this.todos[index].done;
+    },
+
+    rmDone() {
+      this.todos.forEach((todo, index) => {
+        if (todo.done) {
+          this.todos.splice(index, 1);
+        }
+      });
+    },
+
+    filteredTodos() {
+      if (this.filterOption === "all") {
+        return this.todos;
+      } else {
+        const done = this.filterOption === "true";
+        return this.todos.filter((todo) => todo.done === done);
+      }
+    },
   },
 }).mount("#app");
